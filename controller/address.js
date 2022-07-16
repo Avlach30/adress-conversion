cc2fbd5f9283b2486c299f125328ced8e6c2bf5d
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-07-16 18:43:12 +0700

    develop handler or controller for API of get all sub district from city id

--- controller/address.js
+++ controller/address.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
 const axios = require('axios').default;
 
-dotenv.config();
-
 const arrayController = require('../utility/getArrayElement.js');
+
+dotenv.config();
 
 exports.getAddress = async (req, res, next) => {
   try {
@@ -55,6 +55,48 @@ exports.getAddress = async (req, res, next) => {
     res.status(200).json({
       status: "success",
       data: address,
+    })
+
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.getSubDistricts = async (req, res, next) => {
+  try {
+    const { kota_id } = req.body;
+
+    if (!kota_id) {
+      const error = new Error('Please, input city id must be required');
+      error.statusCode = 400;
+      throw error;
+    }
+
+    //* Implementing permission of get all sub district from logged user
+    if (req.userId != '62d25d2ee1756b2409946f8f') {
+      const error = new Error('Forbidden to access');
+      error.statusCode = 403;
+      throw error;
+    }
+
+    const allAddress = await axios.get(process.env.ALL_ADDRESS_URL);
+    const { address_kecamatan } = allAddress.data;
+
+    const getSubDistricts = arrayController.filterItems(address_kecamatan, kota_id);
+
+    //* If empty or not fount
+    if (getSubDistricts.length < 1) {
+      const error = new Error('Data not found');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: getSubDistricts,
     })
 
   } catch (error) {
