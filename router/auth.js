0f0f3d9ece290aa425c3abbf8499494c3f944933
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-07-16 13:58:18 +0700

    initialize validation for signup endpoint

--- router/auth.js
+++ router/auth.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const authController = require('../controller/auth.js');
 
 const router = express.Router();
 
-router.post('/signup', authController.signUp);
+router.post(
+  '/signup', 
+  body('email').isEmail().withMessage('Please, input a valid email'),
+  body('name').trim().not().isEmpty().withMessage('Please, input name required'),
+  body('password').trim().isLength({min: 8}).withMessage('Please, required minimum 8 character for password'),
+  authController.signUp);
 
 module.exports = router;
